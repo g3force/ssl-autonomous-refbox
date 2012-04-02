a6d8c7e464068c23f570822c41fc2d0593e7fb8b
Felix S. Pistorius <mail@felix-pistorius.de>
2012-04-02 17:37:03 +0200

    qmake

--- ssl-refbox/ui_GuiPropertiesDlg.h
+++ ssl-refbox/ui_GuiPropertiesDlg.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'GuiPropertiesDlg.ui'
 **
-** Created: Mon Apr 2 16:40:50 2012
+** Created: Mon Apr 2 17:34:58 2012
 **      by: Qt User Interface Compiler version 4.7.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
